a6d2781f0d7280b9a6490ae89e09c186d79a9a96
Giorgio Aquino <giorgio.aquino@ynap.com>
2016-11-11 15:26:16 +0100

    CLI commands

---
+++ cli/cli-files.js
@@ -0,0 +1,19 @@
+const checksolution = require('../index');
+const program = require('commander');
+
+program
+  //.option('-f, --files', 'force installation')
+  .parse(process.argv);
+let files = program.args;
+
+  if (!files.length) {
+    console.error('One or more path required');
+    process.exit(1);
+  }
+
+  files.forEach(function(pkg){
+    console.log('  install : %s', pkg);
+  });
+  console.log();
+
+checksolution.checkFiles(files);
